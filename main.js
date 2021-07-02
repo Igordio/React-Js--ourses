74a8603cba12580bd7f45e6f9b5e07a98a2e2782
Igordio <drozdnikigor@gmail.com>
2021-07-02 18:36:52 +0300

    Added html

---
+++ main.js
@@ -0,0 +1,8 @@
+fetch('data.json')
+        .then((response) => {
+            return response.json();
+    })
+    .then((data) => {
+        var body = document.getElementById('app')
+        // console.log(data);
+    });
