0432dc39de1971e84243097858d97fe97ad9a827
igord_collab <igor.drozdnik@collab.eu.com>
2021-07-08 19:56:24 +0300

    Finished lesson 1

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-fetch('test.html')
-    .then((response) => {
-        return response.text();
-    })
-    .then((data) => {
-        var body = document.getElementById('app')
+// fetch('test.html')
+//     .then((response) => {
+//         return response.text();
+//     })
+//     .then((data) => {
+//         var body = document.getElementById('app')
 
-        var p = document.createElement('p')
-        p.innerHTML = data
-        boby.appendChild(p)
-    });
+//         var p = document.createElement('p')
+//         p.innerHTML = data
+//         boby.appendChild(p)
+//     });
 
 
     // a = prompt('Введите преаое число');
